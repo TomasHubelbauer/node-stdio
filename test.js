bef2c0cb0bbcd431589ab471311123900d3be24e
Tomas Hubelbauer <tomas@hubelbauer.net>
2020-11-14 12:00:46 +0100

    Demonstrate communicating with a process using stdio

---
+++ test.js
@@ -0,0 +1,17 @@
+import readline from 'readline';
+
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+function ask(/** @type {string} */ question) {
+  return new Promise(answer => rl.question(question + ' ', answer));
+}
+
+void async function () {
+  console.log('Your favorite color is', await ask('What is your favorite color?'));
+  console.log('Your favorite animal is', await ask('What is your favorite animal?'));
+  console.log('Your favorite season is', await ask('What is your favorite season?'));
+  console.log('Your favorite number is', await ask('What is your favorite number?'));
+
+  console.log('Thank you.');
+  rl.close();
+}()
