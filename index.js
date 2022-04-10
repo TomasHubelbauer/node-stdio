11944c051eacccc295dda5c907508f427c8c8245
Tomas Hubelbauer <tomas@hubelbauer.net>
2022-04-10 19:21:19 +0200

    Remove void async wrappers and use TLA
    It is no longer needed in recent versions of node with top-level await support.

--- index.js
+++ index.js
@@ -1,59 +1,57 @@
 import child_process from 'child_process';
 import stream from 'stream';
 
-void async function () {
-  const cp = child_process.exec('node test');
+const cp = child_process.exec('node test');
 
-  function defer() {
-    let resolve;
-    let reject;
-    const promise = new Promise((_resolve, _reject) => {
-      resolve = _resolve;
-      reject = _reject;
-    });
-
-    return { promise, resolve, reject };
-  }
-
-  let deferred = defer();
-
-  /** @this {stream.Readable} */
-  async function read(/** @type {number} */ size) {
-    if (!deferred) {
-      return;
-    }
-
-    this.push(await deferred.promise + '\n');
-  }
-
-  cp.stdout.on('data', chunk => {
-    // Note that either `answer⏎question ` or just `question ` chunks might come
-    for (const line of chunk.split('\n')) {
-      switch (line) {
-        case 'What is your favorite color? ': {
-          deferred.resolve('blue');
-          deferred = defer();
-          break;
-        }
-        case 'What is your favorite animal? ': {
-          deferred.resolve('rabbit');
-          deferred = defer();
-          break;
-        }
-        case 'What is your favorite season? ': {
-          deferred.resolve('autumn');
-          deferred = defer();
-          break;
-        }
-        case 'What is your favorite number? ': {
-          deferred.resolve('seven');
-          deferred = null;
-          break;
-        }
-      }
-    }
+function defer() {
+  let resolve;
+  let reject;
+  const promise = new Promise((_resolve, _reject) => {
+    resolve = _resolve;
+    reject = _reject;
   });
 
-  cp.stdout.pipe(process.stdout);
-  new stream.Readable({ read }).pipe(cp.stdin);
-}()
+  return { promise, resolve, reject };
+}
+
+let deferred = defer();
+
+/** @this {stream.Readable} */
+async function read(/** @type {number} */ size) {
+  if (!deferred) {
+    return;
+  }
+
+  this.push(await deferred.promise + '\n');
+}
+
+cp.stdout.on('data', chunk => {
+  // Note that either `answer⏎question ` or just `question ` chunks might come
+  for (const line of chunk.split('\n')) {
+    switch (line) {
+      case 'What is your favorite color? ': {
+        deferred.resolve('blue');
+        deferred = defer();
+        break;
+      }
+      case 'What is your favorite animal? ': {
+        deferred.resolve('rabbit');
+        deferred = defer();
+        break;
+      }
+      case 'What is your favorite season? ': {
+        deferred.resolve('autumn');
+        deferred = defer();
+        break;
+      }
+      case 'What is your favorite number? ': {
+        deferred.resolve('seven');
+        deferred = null;
+        break;
+      }
+    }
+  }
+});
+
+cp.stdout.pipe(process.stdout);
+new stream.Readable({ read }).pipe(cp.stdin);
--- test.js
+++ test.js
@@ -6,12 +6,10 @@ function ask(/** @type {string} */ question) {
   return new Promise(answer => rl.question(question + ' ', answer));
 }
 
-void async function () {
-  console.log('Your favorite color is', await ask('What is your favorite color?'));
-  console.log('Your favorite animal is', await ask('What is your favorite animal?'));
-  console.log('Your favorite season is', await ask('What is your favorite season?'));
-  console.log('Your favorite number is', await ask('What is your favorite number?'));
+console.log('Your favorite color is', await ask('What is your favorite color?'));
+console.log('Your favorite animal is', await ask('What is your favorite animal?'));
+console.log('Your favorite season is', await ask('What is your favorite season?'));
+console.log('Your favorite number is', await ask('What is your favorite number?'));
 
-  console.log('Thank you.');
-  rl.close();
-}()
+console.log('Thank you.');
+rl.close();
